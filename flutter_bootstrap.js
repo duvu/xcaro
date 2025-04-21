dea4898f34cf04927802cef6df3ab19fcaba586b
duvu <hoaivubk@gmail.com>
2025-04-22 01:07:56 +0700

    Updated with assets

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "12034130"
+    serviceWorkerVersion: "2745807174"
   }
 });
